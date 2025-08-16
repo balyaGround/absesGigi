d3a6bb7d415536ee746f14da62db89836f5fe029
deliyanasiagian <deliyanasiagian@gmail.com>
2025-08-16 19:43:15 +0700

    ubah gejal dan bobot

--- src/data/bobotAbses.ts
+++ src/data/bobotAbses.ts
@@ -1,19 +1,27 @@
 export const gejalaList = [
   "Demam",
   "Bau Mulut",
-  "Rasa sakit yang berdenyut pada gusi atau gigi",
+  "Rasa sakit yang berdenyut pada gigi atau gusi",
   "Rasa sakit yang menjalar ke telinga, rahang, dan leher",
-  "Gusi bengkak, merah dan mengilap",
-  "Pembengkakan kelenjar getah bening",
-  "Pembengkakan area di gigi yang terinfeksi",
+  "Rasa sakit yang lebih parah saat berbaring",
+  "Gusi bengkak, merah, dan mengilap",
+  "Ngilu saat mengonsumsi makanan dan minuman",
+  "Pembengkakan kelenjar getah bening (bawah rahang)",
+  "Wajah membengkak",
+  "Pembengkakan pada area sekitar gigi yang terinfeksi",
+  "Sesak Nafas",
   "Keluar nanah dari benjolan pada gusi",
-  "Gigi goyang",
+  "Gigi Goyang",
+  "Gusi Berdarah Ketika disentuh",
+  "Terdapat Plak Pada Gigi (Karang gigi)",
+  "Merokok",
+  "Suka Mengonsumsi Makanan dan Minuman Yang Manis",
 ];
 
 export const bobotAbses: Record<string, number[]> = {
-  "Abses Periodental": [3, 10, 2, 0, 5, 1, 0, 0, 10],
-  "Abses Periapikal": [1, 6, 10, 8, 1, 5, 9, 10, 5],
-  "Abses Gingiva": [6, 8, 4, 2, 8, 0, 0, 10, 3],
+  "Abses Periodental": [5, 9, 8, 6, 4, 9, 5, 6, 5, 8, 3, 8, 8, 7, 8, 4, 5],
+  "Abses Periapikal": [4, 7, 8, 7, 5, 5, 4, 5, 4, 8, 3, 7, 6, 5, 6, 3, 6],
+  "Abses Gingiva": [4, 7, 6, 5, 3, 8, 4, 4, 3, 6, 2, 8, 4, 8, 8, 4, 4],
 };
 
 export const solusiAbses: Record<string, string> = {
